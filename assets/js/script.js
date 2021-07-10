6105e824d0c9c3df59b47e1209f689ca38fdf8ef
Olga <o.smolyakova30@gmail.com>
2021-07-10 12:41:59 -0500

    fixed function for current weather, now all data display on my card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 let cityInput = document.querySelector('#city-input');
 let searchBtn = document.querySelector('#search-btn');
-let cityName = document.querySelector('#city-name');
-let currentDate = document.querySelector('#current-date');
+let cityNameDisplay = document.querySelector('#city-name');
+let currentDateDisplay = document.querySelector('#current-date');
 let recentCities = document.querySelector('#recent-cities');
 let cityArr = [];
 let formattedCityInput;
+var currentDate = moment().format("dddd, MMMM Do");
 const apiKey = '5569f0d8093687922f5c0ba190e02e6c'; // do not forget remove it before push
 
-
+function displayDates() {
+    // display current date
+    currentDateDisplay.textContent = currentDate
+    // display future forecast dates
+    for (let i = 0; i < 5; i++) {
+        let forecastDate = $(".date")
+        forecastDate[i].textContent = moment().add(i, "d").format("dddd, MMM Do");
+    }
+}
 //function to fromat city Input and add alert if input is empty
 let inputHandeler = function(event){
     event.preventDefault();
@@ -32,7 +41,7 @@ let inputHandeler = function(event){
 };
 
 let getCityForecast5days = function(cityName){
-   let URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+   let URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
    fetch(URL).then(function(response){
        if (response.ok){
         response.json().then(function(data){
@@ -42,18 +51,48 @@ let getCityForecast5days = function(cityName){
         } 
       )}
    }) 
+   displayDates();
+
 };
 
 let getCityForecastGetCurrent = function(cityName){
-    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    cityNameDisplay.textContent = cityName;
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
     fetch(URL).then(function(response){
         if (response.ok){
          response.json().then(function(data){
              console.log(data);
              
+             let currentTempDisplay = document.querySelector("#current-temp");
+             currentTempDisplay.textContent = Math.round(data.main.temp);
+
+             let currentWindDisplay = document.querySelector("#current-wind");
+             currentWindDisplay.textContent = Math.round(data.wind.speed);
+
+             let currentHumidityDisplay = document.querySelector("#current-humidity");
+             currentHumidityDisplay.textContent = Math.round(data.main.humidity);
+
+             var cityLat = data.coord.lat
+             var cityLon = data.coord.lon
+             var uvApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${apiKey}`
+            fetch(uvApi)
+            .then(response => response.json())
+            .then(data => {
+                var currentUvi = Math.round(data.current.uvi)
+                var currentUviDisplay = document.querySelectorAll("#current-uvi")
+                currentUviDisplay.forEach(node =>{
+                    node.textContent = currentUvi;
+                })
+            })
+  console.log(data)
          } 
        )}
     }) 
+
+    
+
+
+    displayDates();
  };
 
 
@@ -72,9 +111,7 @@ let saveCity = function(city){
         buttonEl.innerHTML = city;
         recentCities.appendChild(buttonEl);
     }
-    // if (localStorage.getItem("cities")) {
-    //     cityArr = JSON.parse(localStorage.getItem("cities"))
-    // }
+   
     localStorage.setItem('cities', JSON.stringify(cityArr)); 
 }
 
