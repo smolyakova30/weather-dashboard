453d3fa501573a9d3c35a594dc7d73c652dad2f4
Olga <o.smolyakova30@gmail.com>
2021-07-10 16:19:47 -0500

    added fucntion which triger serach if you click on recent cities buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,20 +40,6 @@ let inputHandeler = function(event){
     console.log(formattedCityInput); // needs to be deleted
 };
 
-let getCityForecast5days = function(cityName){
-   let URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
-   fetch(URL).then(function(response){
-       if (response.ok){
-        response.json().then(function(data){
-
-            console.log(data);
-
-        } 
-      )}
-   }) 
-   displayDates();
-
-};
 
 let getCityForecastGetCurrent = function(cityName){
     cityNameDisplay.textContent = cityName;
@@ -82,6 +68,14 @@ let getCityForecastGetCurrent = function(cityName){
                 var currentUviDisplay = document.querySelectorAll("#current-uvi")
                 currentUviDisplay.forEach(node =>{
                     node.textContent = currentUvi;
+                    currentUvi = parseInt(currentUvi)
+                    if (currentUvi <=2){
+                        document.querySelector("#current-uvi").classList.add("low")
+                    }else if (currentUvi>=3 && currentUvi<=5){
+                        document.querySelector("#current-uvi").classList.add("moderate")
+                    }else{
+                        document.querySelector("#current-uvi").classList.add("high")
+                    }
                 })
             })
   console.log(data)
@@ -89,10 +83,77 @@ let getCityForecastGetCurrent = function(cityName){
        )}
     }) 
 
-    
-
-
     displayDates();
+ };
+
+ let getCityForecast5days = function(cityName){
+    let URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+    fetch(URL).then(function(response){
+        if (response.ok){
+         response.json().then(function(data){
+ 
+             console.log(data);
+             for (let i=0; i<5; i++) {
+                 console.log('we are looping this is turn: ' + i)
+                 console.log(data.list[(i*9)+3].main.temp)
+                 console.log(data.list[(i*9)+3].wind.speed) 
+                 console.log(data.list[(i*9)+3].main.humidity) 
+                 console.log(data.list[(i*9)+3].weather[0].icon)     
+             }
+             //day 1
+             let currentIcon1 = document.querySelector("#icon-1");
+             currentIcon1.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png`);
+             let currentTempDisplay1 = document.querySelector("#high-1");
+             currentTempDisplay1.textContent = Math.round(data.list[3].main.temp);
+             let currentWindDisplay1 = document.querySelector("#low-1");
+             currentWindDisplay1.textContent = Math.round(data.list[3].wind.speed);
+             let currentHumidityDisplay1 = document.querySelector("#humidity-1");
+             currentHumidityDisplay1.textContent = Math.round(data.list[3].main.humidity);
+             
+             //day 2
+             let currentIcon2 = document.querySelector("#icon-2");
+             currentIcon2.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[12].weather[0].icon}.png`);
+             let currentTempDisplay2 = document.querySelector("#high-2");
+             currentTempDisplay2.textContent = Math.round(data.list[12].main.temp);
+             let currentWindDisplay2 = document.querySelector("#low-2");
+             currentWindDisplay2.textContent = Math.round(data.list[12].wind.speed);
+             let currentHumidityDisplay2 = document.querySelector("#humidity-2");
+             currentHumidityDisplay2.textContent = Math.round(data.list[12].main.humidity);
+
+             //day 3
+             let currentIcon3 = document.querySelector("#icon-3");
+             currentIcon3.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[21].weather[0].icon}.png`);
+             let currentTempDisplay3 = document.querySelector("#high-3");
+             currentTempDisplay3.textContent = Math.round(data.list[21].main.temp);
+             let currentWindDisplay3 = document.querySelector("#low-3");
+             currentWindDisplay3.textContent = Math.round(data.list[21].wind.speed);
+             let currentHumidityDisplay3 = document.querySelector("#humidity-3");
+             currentHumidityDisplay3.textContent = Math.round(data.list[21].main.humidity);
+
+             //day 4
+             let currentIcon4 = document.querySelector("#icon-4");
+             currentIcon4.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[30].weather[0].icon}.png`);
+             let currentTempDisplay4 = document.querySelector("#high-4");
+             currentTempDisplay4.textContent = Math.round(data.list[30].main.temp);
+             let currentWindDisplay4 = document.querySelector("#low-4");
+             currentWindDisplay4.textContent = Math.round(data.list[30].wind.speed);
+             let currentHumidityDisplay4 = document.querySelector("#humidity-4");
+             currentHumidityDisplay4.textContent = Math.round(data.list[30].main.humidity);
+
+             //day 5
+             let currentIcon5 = document.querySelector("#icon-5");
+             currentIcon5.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[39].weather[0].icon}.png`);
+             let currentTempDisplay5 = document.querySelector("#high-5");
+             currentTempDisplay5.textContent = Math.round(data.list[39].main.temp);
+             let currentWindDisplay5 = document.querySelector("#low-5");
+             currentWindDisplay5.textContent = Math.round(data.list[39].wind.speed);
+             let currentHumidityDisplay5 = document.querySelector("#humidity-5");
+             currentHumidityDisplay5.textContent = Math.round(data.list[39].main.humidity);
+         } 
+       )}
+    }) 
+    displayDates();
+ 
  };
 
 
@@ -115,6 +176,7 @@ let saveCity = function(city){
     localStorage.setItem('cities', JSON.stringify(cityArr)); 
 }
 
+
 let keepCities = function(){
     cityArr = JSON.parse(localStorage.getItem('cities'));
     
@@ -135,7 +197,16 @@ let keepCities = function(){
 
 keepCities();
 
-
+let searchAgain = function(){
+    getCityForecast5days(event.target.textContent);
+    getCityForecastGetCurrent(event.target.textContent);
+}
 
 //eventListener
 searchBtn.addEventListener('click', inputHandeler)
+$('.recentCity').on("click", searchAgain);
+ 
+
+
+
+
